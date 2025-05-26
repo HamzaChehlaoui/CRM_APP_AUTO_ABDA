35958092d1ba4d81bf41b17d8a11454ef2dd4642
Hamza <hamzacoude@gmail.com>
2025-05-26 15:04:28 +0000

    Add migration for entretiens table with relationships to Car, Client, and User

---
+++ database/migrations/2025_05_26_143446_create_entretiens_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('entretiens', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('car_id')->nullable()->constrained('cars')->onDelete('set null');
+        $table->foreignId('client_id')->nullable()->constrained('clients')->onDelete('set null');
+        $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('set null');
+        $table->string('type'); 
+        $table->date('scheduled_at');
+        $table->text('description')->nullable();
+        $table->enum('status', ['planifié', 'réalisé', 'annulé'])->default('planifié');
+        $table->timestamps();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('entretiens');
+    }
+};
