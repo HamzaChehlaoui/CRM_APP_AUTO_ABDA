2fd83b541200d595cb69f367e118d30c3071c960
Hamza <hamzacoude@gmail.com>
2025-05-26 10:46:04 +0000

    Create migration for clients table with necessary fields and relationships

---
+++ database/migrations/2025_05_25_143331_create_clients_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+       Schema::create('clients', function (Blueprint $table) {
+        $table->id();
+        $table->string('full_name')->comment('Nom complet');
+        $table->string('phone')->comment('Téléphone');
+        $table->string('cin')->unique()->comment('Carte d\'identité nationale');
+        $table->string('address')->nullable()->comment('Adresse');
+        $table->string('email')->nullable()->comment('Email');
+
+        $table->foreignId('branch_id')->constrained('branches')->onDelete('cascade')->comment('Agence / Succursale');
+        $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null')->comment('Utilisateur qui a enregistré le client');
+
+        $table->timestamps();
+});
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
