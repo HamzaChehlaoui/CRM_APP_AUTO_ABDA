64c86d94aaa4ecebdc7f7b4052da05484630db6d
Hamza <hamzacoude@gmail.com>
2025-05-26 16:10:27 +0000

    Add UpdateSuiviRequest for suivi update validation with rules and authorization

---
+++ app/Http/Requests/UpdateSuiviRequest.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateSuiviRequest extends FormRequest
+{
+    public function authorize() { return true; }
+    public function rules()
+    {
+        return [
+            'client_id' => 'required|exists:clients,id',
+            'user_id' => 'nullable|exists:users,id',
+            'note' => 'required|string',
+            'date' => 'required|date',
+        ];
+    }
+}
