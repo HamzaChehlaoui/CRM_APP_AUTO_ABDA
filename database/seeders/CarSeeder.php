857d06170d9d530c0ba8ddc9936daebbdf16a30b
Hamza <hamzacoude@gmail.com>
2025-05-26 15:04:40 +0000

    Add CarSeeder to populate car data with brands and models

---
+++ database/seeders/CarSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Car;
+use Faker\Factory;
+
+class CarSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $faker = Factory::create('fr_FR');
+        $brands = ["Toyota", "Renault", "Peugeot", "Hyundai", "Kia", "Volkswagen", "Dacia", "Ford", "Fiat", "Nissan"];
+        $models = ["Yaris", "Clio", "208", "i20", "Sportage", "Golf", "Logan", "Focus", "Punto", "Micra"];
+
+        foreach (range(0, 9) as $i) {
+            Car::create([
+                'brand' => $brands[$i],
+                'model' => $models[$i],
+                'ivn' => $faker->unique()->bothify('IVN#####'),
+                'registration_number' => $faker->unique()->bothify('??-####-??'),
+                'chassis_number' => $faker->unique()->bothify('CH########'),
+                'color' => $faker->safeColorName(),
+                'year' => $faker->numberBetween(2015, 2024),
+            ]);
+        }
+    }
+}
