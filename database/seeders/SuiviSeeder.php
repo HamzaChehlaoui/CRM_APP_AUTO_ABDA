29833c05cc978513009becd65994f5f0e0541d1d
Hamza <hamzacoude@gmail.com>
2025-05-26 15:05:24 +0000

    Add SuiviSeeder to populate suivi data with random client IDs, user IDs, notes, statuses, and follow-up dates

---
+++ database/seeders/SuiviSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Suivi;
+use App\Models\Client;
+use App\Models\User;
+use Faker\Factory;
+
+class SuiviSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $faker = Factory::create('fr_FR');
+        $clients = Client::pluck('id')->toArray();
+        $users = User::pluck('id')->toArray();
+
+        foreach (range(1, 10) as $i) {
+            Suivi::create([
+                'client_id' => $faker->randomElement($clients),
+                'user_id' => $faker->randomElement($users),
+                'note' => $faker->sentence(),
+                'status' => $faker->randomElement(['en_cours', 'termine', 'annule']),
+                'date_suivi' => $faker->dateTimeBetween('-1 year', 'now'),
+            ]);
+        }
+    }
+}
