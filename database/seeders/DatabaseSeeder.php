a96df2d232b308c4cfd304cddb3ba84a4cb6a324
Hamza <hamzacoude@gmail.com>
2025-05-26 15:04:52 +0000

    Refactor DatabaseSeeder to include additional seeders for Invoice, Suivi, Notification, Entretien, and Reclamation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,15 +3,26 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\{Role, Branch, User, Client, Car, Invoice, Suivi, Notification, Entretien, Reclamation};
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        // Call seeders in order
         $this->call([
             RolesTableSeeder::class,
             BranchesTableSeeder::class,
             UsersTableSeeder::class,
+            ClientSeeder::class,
+            CarSeeder::class,
+            InvoiceSeeder::class,
+            SuiviSeeder::class,
+            NotificationSeeder::class,
+            EntretienSeeder::class,
+            ReclamationSeeder::class,
         ]);
     }
 }
