09e3ff16842ca5455edd983fabb4cf6bb9bc60dc
Hamza <hamzacoude@gmail.com>
2025-05-26 15:04:46 +0000

    Add ClientSeeder to populate client data with random names, contacts, and relationships

---
+++ database/seeders/ClientSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Client;
+use App\Models\User;
+use App\Models\Branch;
+use Faker\Factory;
+
+class ClientSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $faker = Factory::create('fr_FR');
+        $branches = Branch::pluck('id')->toArray();
+        $users = User::pluck('id')->toArray();
+
+        foreach (range(1, 10) as $i) {
+            Client::create([
+                'full_name' => $faker->name,
+                'phone' => $faker->phoneNumber,
+                'cin' => $faker->unique()->bothify('????#####'),
+                'address' => $faker->address,
+                'email' => $faker->email,
+                'branch_id' => $faker->randomElement($branches),
+                'created_by' => $faker->randomElement($users),
+            ]);
+        }
+    }
+}
