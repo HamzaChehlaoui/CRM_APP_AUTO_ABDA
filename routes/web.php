a7e5ee3da2be43298fab513dd7be381bab5257cc
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-06-01 15:22:22 +0000

    Fix route definition for storing suivis in web.php

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
+    Route::post('/suivis', [SuiviController::class, 'store'])->name('suivis.store');
     Route::get('/clients/post-sale-stats', [DashboardController::class, 'postSaleStats']);
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
