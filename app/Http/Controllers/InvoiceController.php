cf6fbfe99d41c1c37839cc96f2875b733fa2c918
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-06-04 17:51:09 +0000

    Refactor index method in InvoiceController to integrate DashboardService for enhanced data retrieval and display in invoices view

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -5,14 +5,31 @@ namespace App\Http\Controllers;
 use App\Models\Invoice;
 use App\Http\Requests\StoreInvoiceRequest;
 use App\Http\Requests\UpdateInvoiceRequest;
+use Illuminate\Http\Request;
+use App\Services\DashboardService;
 
 class InvoiceController extends Controller
 {
-    public function index()
-    {
-        $invoices = Invoice::with(['client', 'car', 'user', 'branch'])->paginate(10);
-        return view('invoices.index', compact('invoices'));
-    }
+    public function index(Request $request, DashboardService $dashboardService)
+{
+    $user = auth()->user();
+    $selectedBranch = $request->get('branch_filter', 'all');
+    $data = $dashboardService->resolveBranchInfo($user, $selectedBranch);
+    $stats = $dashboardService->getDashboardStats(
+        $data['clientsQuery'],
+        $data['suivisQuery'],
+        $data['invoicesQuery']
+    );
+    $clients = $data['clientsQuery']
+    ->with('cars')
+    ->get();
+
+    return view('page.factures', array_merge($stats, [
+        'branches' => $data['branches'],
+        'selectedBranch' => $selectedBranch,
+        'clients' =>$clients,
+    ]));
+}
 
     public function create()
     {
