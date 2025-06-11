7afb1ae77464301c644caf78db37a75802c72bcc
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-06-11 17:39:41 +0100

    Add AllDataExport class to handle multiple sheets export functionality

---
+++ app/Exports/AllDataExport.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\WithMultipleSheets;
+
+class AllDataExport implements WithMultipleSheets
+{
+    use Exportable;
+
+    protected array $sheets;
+
+    public function __construct(array $sheets)
+    {
+        $this->sheets = $sheets;
+    }
+
+    /**
+     * @return array
+     */
+    public function sheets(): array
+{
+    $sheetsToExport = [];
+    foreach ($this->sheets as $sheetName => $sheetData) {
+        if (!empty($sheetData['fields'])) {
+            $sheetsToExport[ucfirst($sheetName)] = new GenericExport(
+                $sheetData['data'], 
+                $sheetData['headings'],
+                $sheetData['fields']
+            );
+        }
+    }
+    return $sheetsToExport;
+}
+}
