5c8a0372cdfac65aaa983109f6f2fa020497bfde
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-06-21 09:33:11 +0100

    refactor: Enhance Google Drive integration in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,14 @@
 
 namespace App\Providers;
 
-
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use League\Flysystem\Filesystem;
+use Masbug\Flysystem\GoogleDriveAdapter;
+use Google\Client;
+use Google\Service\Drive;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,11 +26,28 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-         View::composer('*', function ($view) {
-        if (Auth::check()) {
-            $user = Auth::user();
-            $view->with('unreadCount', $user->unreadNotifications->count());
-        }
-    });
+        //  conect Google Drive
+        Storage::extend('google-drive-ext', function ($app, $config) {
+            $client = new Client();
+            $client->setClientId($config['clientId']);
+            $client->setClientSecret($config['clientSecret']);
+            $client->setAccessType('offline');
+            $client->setApprovalPrompt('force');
+            $client->setScopes(['https://www.googleapis.com/auth/drive']);
+            $client->refreshToken($config['refreshToken']);
+            $client->fetchAccessTokenWithRefreshToken($config['refreshToken']);
+
+            $service = new Drive($client);
+            $adapter = new GoogleDriveAdapter($service, $config['folderId'] ?? null);
+
+            return new Filesystem($adapter);
+        });
+
+        View::composer('*', function ($view) {
+            if (Auth::check()) {
+                $user = Auth::user();
+                $view->with('unreadCount', $user->unreadNotifications->count());
+            }
+        });
     }
 }
