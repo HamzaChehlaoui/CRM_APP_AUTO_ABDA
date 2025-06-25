5d341dd24d6ff2720739402a4b6d3a4787810e3c
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-06-25 16:21:28 +0100

    Fix whitespace issue in edit-invoice modal for improved code consistency

--- resources/views/livewire/partials/modals/edit-invoice.blade.php
+++ resources/views/livewire/partials/modals/edit-invoice.blade.php
@@ -65,7 +65,7 @@
                                         value="{{ old('car_registration', $car->registration_number) }}"
                                         class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent">
                                 </div>
-                            
+
                                 <div class="md:col-span-2">
                                     <label class="block text-sm font-medium text-gray-700 mb-1">IVN</label>
                                     <input type="text" name="car_ivn" value="{{ old('car_ivn', $car->ivn) }}"
